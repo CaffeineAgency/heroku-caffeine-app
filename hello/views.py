364593e0ec76758ffd836c77e99a081c12d12793
Andrew <mrcluster@yandex.ru>
2017-09-17 22:43:47 +0700

    make it better

--- hello/views.py
+++ hello/views.py
@@ -21,5 +21,33 @@ def db(request):
 
 def test(request):
     import vk_api
-    return HttpResponse('TEST<b>BOLD</b><br>TEST')
+    client_id = '5882810'
+    client_sec = 'umeTZX5ykJVPyNsQSlzf'
+    vk_session = vk_api.VkApi(token="e5fe3e55c448969c1bb62a51429b409863d46199f346401508e01efae41fcaf4bc52be0aded49cf736fd3")
+    perm = "messages,friends,audio,offline,groups,wall"
+    vk_session.authorization()
+    tools = vk_api.VkTools(vk_session)
+    owner = '148147627'
+    items = tools.get_all('wall.get', 100, {'owner_id': int("-" + owner)})["items"]
+    strhtml = ''
+    for item in items:
+        if not "attachments" in item:
+            continue
+        attachments = item["attachments"]
+        for attachment in attachments:
+            if attachment["type"] != "photo":
+                continue
+            photo = attachment["photo"]
+            url = ""
+            if ("photo_1280" in photo):
+                url = photo["photo_1280"]
+            elif ("photo_807" in photo):
+                url = photo["photo_807"]
+            elif ("photo_604" in photo):
+                url = photo["photo_604"]
+            else:
+                continue
+            strhtml += '<img src"' + url + '"/><br>'
+
+    return HttpResponse('strhtml')
 
