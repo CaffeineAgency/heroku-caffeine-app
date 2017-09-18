d1c886233a75440d0a021adeb5b6c808b89d936c
Andrew <mrcluster@yandex.ru>
2017-09-18 22:57:18 +0700

    make it better

--- init/requestWorkers.py
+++ init/requestWorkers.py
@@ -1,7 +1,5 @@
 def getGet(request, name):
     return request.GET.get(name)
 
-
 def getPost(request, name):
     return request.POST.get(name)
-
--- init/urls.py
+++ init/urls.py
@@ -3,15 +3,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
-
-# Examples:
-# url(r'^$', 'init.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
+import site.views
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^test', hello.views.test, name='test'),
+    url(r'^$', site.views.index, name='index'),
+    url(r'^test', site.views.test, name='test'),
     url(r'^admin/', include(admin.site.urls)),
 ]
