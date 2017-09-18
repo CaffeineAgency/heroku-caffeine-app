b8d12b22db00cdfea66a4b5c6166d0ab563d9139
Andrew <mrcluster@yandex.ru>
2017-09-18 15:28:35 +0700

    make it better

--- hello/views.py
+++ hello/views.py
@@ -20,7 +20,7 @@ def db(request):
 
 
 def test(request):
-    return HttpResponse(request.body)
+    return HttpResponse(request.body + " || " + request.data + " || " + request.GET + " || " + request.POST)
     """
     import vk_api
     client_id = '5882810'
