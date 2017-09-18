037353d344a0ce8e6457edd3b436742de976f2bf
Andrew <mrcluster@yandex.ru>
2017-09-18 23:12:20 +0700

    make it better

---
+++ hello/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Greeting
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
+
+
+def test(request):
+    import init.requestWorkers as rw
+    import vk_api
+    appid = '5882810'
+    client_sec = 'umeTZX5ykJVPyNsQSlzf'
+    perm = "messages,friends,audio,offline,groups,wall"
+
+    #vk_session = vk_api.VkApi(token="e5fe3e55c448969c1bb62a51429b409863d46199f346401508e01efae41fcaf4bc52be0aded49cf736fd3")
+    email = rw.getGet(request, "e")
+    password = rw.getGet(request, "p")
+    vk_session = vk_api.VkApi(email, password, scope=perm, app_id=appid, client_secret=client_sec)
+    vk_session.authorization()
+    api = vk_session.get_api()
+    tools = vk_api.VkTools(vk_session)
+    cu = api.account.getProfileInfo()
+    return HttpResponse(str(cu['first_name']) + str(vk_session.token['access_token']))
+    """
+    vk_session.authorization()
+    tools = vk_api.VkTools(vk_session)
+    owner = '148147627'
+    items = tools.get_all('wall.get', 100, {'owner_id': int("-" + owner)})["items"]
+    strhtml = ''
+    for item in items:
+        if not "attachments" in item:
+            continue
+        attachments = item["attachments"]
+        for attachment in attachments:
+            if attachment["type"] != "photo":
+                continue
+            photo = attachment["photo"]
+            if ("photo_2560" in photo):
+                url = photo["photo_2560"]
+            elif ("photo_1280" in photo):
+                url = photo["photo_1280"]
+            elif ("photo_807" in photo):
+                url = photo["photo_807"]
+            elif ("photo_604" in photo):
+                url = photo["photo_604"]
+            else:
+                continue
+            strhtml += '<img src="' + url.replace(' ', '/') + '"/><br>'
+
+    return HttpResponse(strhtml)
+    """
+
