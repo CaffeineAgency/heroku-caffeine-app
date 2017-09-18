9cd262cce0728fe521f5baa25fea03526db2b91a
Andrew <mrcluster@yandex.ru>
2017-09-18 23:34:11 +0700

    make it better

--- init/urls.py
+++ init/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import site.views
+import hello.views
 
 urlpatterns = [
-    url(r'^$', site.views.index, name='index'),
-    url(r'^test', site.views.test, name='test'),
+    url(r'^$', hello.views.index, name='index'),
+    url(r'^test', hello.views.test, name='test'),
     url(r'^admin/', include(admin.site.urls)),
 ]
