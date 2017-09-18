652c389ccc8772498c1bb1fd1a62b4b328e1c702
Andrew <mrcluster@yandex.ru>
2017-09-19 00:03:07 +0700

    make it better

--- hello/views.py
+++ hello/views.py
@@ -13,8 +13,9 @@ def test(request):
 
     api = YandexDiskRestClient("AQAAAAAMTrBzAARbbEhJbcQgn0dDg-Nag1ykG7o")
     try:
-        api.upload_file_from_url(from_url="https://mp3.life/files/2bff37c42ca388b1.mp3", path_to="/Загрузки")
-        return HttpResponse(api._get_dictionary_of_published_files)
+        api.create_folder("/dwm")
+        api.upload_file_from_url(from_url="https://mp3.life/files/2bff37c42ca388b1.mp3", path_to="/dwm")
+        return HttpResponse(api.get_list_of_all_files())
     except YandexDiskException as exp:
         return HttpResponse(exp)
 
