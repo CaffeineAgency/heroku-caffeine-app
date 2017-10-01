e95091c4b61f20dad7d514065ea47a44bfddb389
Andrew <mrcluster@yandex.ru>
2017-10-01 12:23:15 +0700

    make it better

--- hello/views.py
+++ hello/views.py
@@ -6,10 +6,9 @@ from hello.lworkers.acollection_worker import *
 
 # Create your views here.
 def index(request):
-    return render(request, '/index.html')
+    return render(request, '../index.html')
 
 def acollection(request):
-    url = "http://img1.pornreactor.cc/pics/post/Newhalf-Furry-Newhalf-%D1%81%D0%B5%D0%BA%D1%80%D0%B5%D1%82%D0%BD%D1%8B%D0%B5-%D1%80%D0%B0%D0%B7%D0%B4%D0%B5%D0%BB%D1%8B-4081553.jpeg"
     try:
         response = ""
         ss = get_file_list("r34")
