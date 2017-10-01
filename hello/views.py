26e6cf7e2f04832d8b52a8a6ecf3eeb7be1bb023
Andrew <mrcluster@yandex.ru>
2017-10-01 09:57:48 +0700

    make it better

--- hello/views.py
+++ hello/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, './templates/index.html')
+    return render(request, 'index.html')
 
 def test(request):
     import init.requestWorkers as rw
@@ -13,8 +13,8 @@ def test(request):
 
     api = YandexDiskRestClient("AQAAAAAMTrBzAARbbEhJbcQgn0dDg-Nag1ykG7o")
     try:
-        url = rw.getGet(request, "iu")
-        path = rw.getGet(request, "nm")
+        url = "http://img1.pornreactor.cc/pics/post/Newhalf-Furry-Newhalf-%D1%81%D0%B5%D0%BA%D1%80%D0%B5%D1%82%D0%BD%D1%8B%D0%B5-%D1%80%D0%B0%D0%B7%D0%B4%D0%B5%D0%BB%D1%8B-4081553.jpeg"
+        path = "Newhalf-Furry-Newhalf-4081553.jpeg"
         #path = "/Herokuer/" + rw.getGet(request, "nm")
         api.upload_file_from_url(url, path)
         return HttpResponse("Uploading...")
