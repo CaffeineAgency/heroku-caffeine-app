8fa55064b4f2498f88807a004322aeafafafb9e9
Andrew <mrcluster@yandex.ru>
2017-10-01 11:57:35 +0700

    make it better

--- init/urls.py
+++ init/urls.py
@@ -7,6 +7,6 @@ import hello.views
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
-    url(r'^acollection', hello.views.acollection, name='test'),
+    url(r'^acollection', hello.views.acollection, name='apicol'),
     url(r'^admin/', include(admin.site.urls)),
 ]
