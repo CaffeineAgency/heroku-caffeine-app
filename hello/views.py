cf22507529489498fa04a41fb160b5b2ee38ed5a
Andrew <mrcluster@yandex.ru>
2017-10-01 11:41:24 +0700

    make it better

--- hello/views.py
+++ hello/views.py
@@ -12,13 +12,18 @@ def acollection(request):
     url = "http://img1.pornreactor.cc/pics/post/Newhalf-Furry-Newhalf-%D1%81%D0%B5%D0%BA%D1%80%D0%B5%D1%82%D0%BD%D1%8B%D0%B5-%D1%80%D0%B0%D0%B7%D0%B4%D0%B5%D0%BB%D1%8B-4081553.jpeg"
     try:
         '''
-        filename = download_file(url)
+
         response = upload_file(filename)
         return HttpResponse(response)
         '''
+        response = ""
         ss = get_file_list("Newhalf")
         for post in ss:
-            return HttpResponse(post.user)
+            for image in post.images_list:
+                url = image
+                filename = download_file(url)
+                response += "{}<br><br><br>\n\n\n".format(upload_file(filename))
+        return HttpResponse(response)
     except YandexDiskException as ex:
         return HttpResponse(ex)
 
