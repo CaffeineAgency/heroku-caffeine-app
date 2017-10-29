1f01fc4ce50260a7b094cd1e98a3717a1e2b32b2
Andrew <mrcluster@ya.ru>
2017-10-29 20:12:41 +0700

    wob 4

--- bots/views.py
+++ bots/views.py
@@ -2,11 +2,17 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import TemplateDoesNotExist
+from django.views.decorators.csrf import csrf_exempt
 
 from init.extensions import getGet
 import jsonpickle
 
+@csrf_exempt
 def index(request):
-    print(request)
-    print(request.POST)
-    return HttpResponse(request.__repr__)
+    inc_data = jsonpickle.decode(request.body)
+    type = inc_data["type"]
+    if type == "confirmation":
+        if inc_data["group_id"] == 140299531:
+            return HttpResponse("401176a7")
+    else:
+        return HttpResponse("PASSED!")
