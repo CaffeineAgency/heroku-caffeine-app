ce6480de7287361b840bbce54fd51ef19a608b62
Andrew <mrcluster@ya.ru>
2017-10-29 19:42:22 +0700

    Interface -> 5 colonn flex; Add bot submodule

---
+++ bots/views.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import TemplateDoesNotExist
+
+from init.extensions import getGet
+import jsonpickle
+
+def index(request):
+    return HttpResponse("EXAMPLE TEXT")
--- init/urls.py
+++ init/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import include, url
-
 from django.contrib import admin
-admin.autodiscover()
 
 import hello.views
+import bots.views
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^acollection', hello.views.acollection, name='apicol'),
+    url(r'^bot', bots.views.index, name='botcol'),
     url(r'^admin/', include(admin.site.urls)),
 ]
