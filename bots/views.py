c23dd331255189ea7537049f451e42f4b7a63add
Andrew <mrcluster@ya.ru>
2017-11-11 21:01:50 +0700

    Working on bot

--- bots/views.py
+++ bots/views.py
@@ -3,23 +3,45 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import TemplateDoesNotExist
 from django.views.decorators.csrf import csrf_exempt
+from vk_api import vk_api
 
 from init.extensions import getGet
 import jsonpickle
 
+commands_list = []
+groupsec = "05f59078d7cfe924415b7f162a1e1eace16a9aaa30f969e51636808ce1d317cb7fc5957f7925c9364c258"
+
 @csrf_exempt
 def index(request):
     try:
+        sess = vk_api.VkApi(app_id=6157263, api_version="5.69", token=groupsec)
         inc_data = jsonpickle.decode(request.body)
         type = inc_data["type"]
         if type == "confirmation":
             if inc_data["group_id"] == 140299531:
                 return HttpResponse("401176a7")
         elif type == "message_new":
-            pass
+            obj = inc_data["object"]
+            sender = obj["user_id"]
+            text = obj["body"]
+            if text.startswith("~/"):
+                sess.message.send(user_id=sender, message="Wait for command recognition...")
+                recognized, parsed_command = try_parse_command(text)
+                if not recognized:
+                    sess.message.send(user_id=sender, message="Command not recognized")
+
         elif type == "group_join":
             pass
         else:
             return HttpResponse("err")
+    except vk_api.VkApiError as e:
+        return HttpResponse(e.args)
     except:
-        return HttpResponse("THIS PAGE FOR BOT! NOT FOR HUMANS!")
+        return HttpResponse("иди нахуй")
+    return HttpResponse("ok")
+
+def try_parse_command(text):
+    command_parts = text.split()
+    if command_parts[0] not in commands_list:
+        return False, None
+    return False, None
