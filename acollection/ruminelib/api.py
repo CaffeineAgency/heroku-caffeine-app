3a1c713e9c7d7a3eadcea2504b093ede3715f58d
Andrew <mrcluster@ya.ru>
2017-12-31 18:48:51 +0700

    Fixing rumineapi

--- acollection/ruminelib/api.py
+++ acollection/ruminelib/api.py
@@ -9,50 +9,31 @@ import requests
 
 
 class RuMineApi():
-    def __init__(self, login=None, password=None, request=None):
+    def __init__(self, request, login=None, password=None):
         self.pagenum = 9999999999999
         self.base_url = "https://ru-minecraft.ru"
         self.login = login
         self.password = password
         self.request = request
-
-    def prepare_uri(self, request=None):
-        if not request:
-            if self.request:
-                request = self.request
-            else:
-                raise Exception("No request dict passed")
-        if request["threadId"] is None:
+        if not self.request["threadId"]:
             raise Exception("No threadId passed")
-        else:
-            try:
-                self.threadId = int(request["threadId"])
-            except:
-                raise Exception("No threadId passed")
-        if request["pagenum"] is None:
-            self.pagenum = 9999999999999
-        else:
-            try:
-                self.pagenum = request["pagenum"]
-            except:
-                pass
-        return "{}/forum/showtopic-{}/page-{}/".format(self.base_url, self.threadId, self.pagenum)
+        self.pagenum = 9999999999999 if not self.request["pagenum"] else self.request["pagenum"]
+        self.threadId = int(self.request["threadId"])
 
     def parse_data(self, soup):
         posts = []
-        for item in soup.find_all('li', class_="msg"):
+        for item in soup.select("li.msg"):
             pid = item.select_one(".msgInfo a").text.replace("#", "")
             sender = item.select_one(".msgAutorInfo .autorInfo p > a").text
-            stri = item.select_one(".msgText td > div").encode('utf-8')
+            stri = str(item.select_one(".msgText td > div"))
             posts.append(Post(pid, sender, stri))
         return Response(posts)
 
-    def get_comments(self, url=None):
+    def get_comments(self):
         try:
-            if self.request is not None:
-                url = self.prepare_uri(self.request)
+            url = f"{self.base_url}/forum/showtopic-{self.threadId}/page-{self.pagenum}/"
             source = requests.get(url)
-            source = codecs.decode(source.content, "windows-1251")
+            source = codecs.decode(source.content, source.encoding)
             soup = bs.BeautifulSoup(source, "html5lib")
             # Sanitize HTML
             [itm.nextSibling.extract() for itm in soup.findAll("div", class_="clr")]
@@ -61,13 +42,6 @@ class RuMineApi():
             [spmark.extract() for spmark in soup.findAll("div", class_="title_spoiler")]
             for spler in soup.findAll("div", class_="text_spoiler"): del(spler['style'])
             rarray = self.parse_data(soup)
-            narray = []
-            for post in rarray.posts:
-                _post = post
-                _post.text = codecs.decode(self.parse_data(soup).posts.pop().text, "UTF-8")
-                narray.append(_post)
-            rarray.posts = narray
-
             return jsonpickle.encode(rarray, unpicklable=False)
         except Exception as ex:
-            return jsonpickle.encode(dict(error=ex.args), unpicklable=False)
+            return jsonpickle.encode(dict(errors=ex.args), unpicklable=False)
