9d9335bedce5d9e9eb2354f180eeeda0ef86cc69
Andrew <mrcluster@ya.ru>
2018-03-18 13:58:49 +0700

    Refactoring...

--- bots/BotController.py
+++ bots/BotController.py
@@ -6,10 +6,10 @@ class BotController:
     def __init__(self, obj, hooker):
         self.commands_list = ["help", "cmds", "wake"]
         self.command_dict = {
-            "help": lambda *_: "**Краткий референс**\nПример использования: .{command_name} arg1|arg2|3|(256)|...",
+            "help": lambda *_: "**Краткий референс**\nПример использования: \n.{cmd} arg1|arg2|<...>",
             "cmds": lambda *_: "Доступные команды:\n" + "\n".join(self.commands_list),
             "wake": lambda *_: "Awaken!",
-            "test_notify": lambda *_: GroupApiHooks.notify_creator("Test notify"),
+            "notify_creator": lambda msg, *_: GroupApiHooks.notify_creator(str(msg)),
         }
 
         self.sender = obj["user_id"]
@@ -19,10 +19,7 @@ class BotController:
 
     def start_checking(self):
         if self.text.startswith("."):
-            try:
-                self.parsed_command_result = self.try_parse_command()
-            except Exception as e:
-                GroupApiHooks.notify_creator("Error(s) happend: " + ", ".join(e.args))
+            self.parsed_command_result = self.try_parse_command()
 
     def try_parse_command(self):
         text = self.text[1:].strip()
@@ -31,11 +28,8 @@ class BotController:
         command_name, *command_args = text.split()
         if command_name not in self.command_dict:
             return "bot@Clyde > Такой команды, увы, нет."
-        command_args = command_args if command_args else None
+        command_args = command_args.split("|") if command_args else None
         return command_dict[command_name](command_args)
 
     def execute(self):
-        send_message = self.hooker.send_message
-        sender = self.sender
-        parsed_command_result = self.parsed_command_result
-        send_message(sender, parsed_command_result)
+        self.hooker.send_message(self.sender, self.parsed_command_result)
--- bots/views.py
+++ bots/views.py
@@ -9,19 +9,24 @@ from bots.GroupApiHooks import GroupApiHooks
 
 @csrf_exempt
 def index(request):
-    inc_data = jsonpickle.decode(request.body)
-    type = inc_data["type"]
-    if type == "confirmation":
-        if inc_data["group_id"] == 140299531:
-            return HttpResponse("401176a7")
-    elif type == "message_new":
-        obj = inc_data["object"]
-        hooker = GroupApiHooks()
-        controller = BotController(obj, hooker)
-        controller.execute()
-    elif type == "group_join":
-        obj = inc_data["object"]
-        user_id, join_type = obj["user_id"], obj["join_type"]
-        text = "Group join: \n" + GroupApiHooks().users_get(user_id)
-        GroupApiHooks.notify_creator(text)
-    return HttpResponse("ok")
+    try:
+        print("bot@Clyde > Ok, we got something: " + request.body)
+        inc_data = jsonpickle.decode(request.body)
+        type = inc_data["type"]
+        if type == "confirmation":
+            if inc_data["group_id"] == 140299531:
+                return HttpResponse("401176a7")
+        elif type == "message_new":
+            obj = inc_data["object"]
+            hooker = GroupApiHooks()
+            controller = BotController(obj, hooker)
+            controller.execute()
+        elif type == "group_join":
+            obj = inc_data["object"]
+            user_id, join_type = obj["user_id"], obj["join_type"]
+            text = "Group join: \n" + GroupApiHooks().users_get(user_id)
+            GroupApiHooks.notify_creator(text)
+    except Exception as e:
+        GroupApiHooks.notify_creator("Error(s) happend: " + ", ".join(e.args))
+    finally:
+        return HttpResponse(content="ok")
