133f432b4d6eb2136d903ecd40b4b6e2c255fcf4
Andrew <mrcluster@ya.ru>
2018-03-31 14:44:40 +0700

    Implementation of Scout

--- init/settings.py
+++ init/settings.py
@@ -15,6 +15,7 @@ TEST_RUNNER = 'init.heroku_test_runner.HerokuDiscoverRunner'
 # Application definition
 
 INSTALLED_APPS = (
+    'scout_apm.django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
