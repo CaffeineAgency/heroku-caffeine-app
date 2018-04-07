4a474ad04177b52daf9ca4e13d37b1b048313706
Andrew <mrcluster@ya.ru>
2018-04-08 00:15:49 +0700

    Removing auth system

--- init/urls.py
+++ init/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
-import cpanel.views
-import cpanel.urls
-import hello.views
 import bots.views
+import hello.views
 
 app_name="nuark-caffeine"
 
@@ -14,6 +12,4 @@ urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^acollection', hello.views.acollection, name='apicol'),
     url(r'^bot', bots.views.index, name='botcol'),
-    url(r'^admin/', admin.site.index, name='adminpanel'),
-    *cpanel.urls.urlpatterns
 ]
