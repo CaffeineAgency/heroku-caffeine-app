e55f51491656f16eae131a9d25a0fbd9be9777c9
Andrew <mrcluster@ya.ru>
2018-04-08 13:00:21 +0700

    Procfile and .gitignore update

--- app.py
+++ app.py
@@ -22,10 +22,5 @@ def bot_route():
     return bots_index(request)
 
 
-def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
