ab39e57a24b052ca774baa2bce44018a8501257f
Andrew <mrcluster@ya.ru>
2018-04-15 01:04:12 +0700

    fix

--- bots/views.py
+++ bots/views.py
@@ -9,17 +9,15 @@ from bots.GroupApiHooks import GroupApiHooks
 def index(request):
     yield "ok"
     _json = getData(request)
-    print(_json)
     if _json:
-        inc_data = jsonpickle.decode(_json)
         gids = {
             140299531: "rkkk_token",
             153656617: "cagency_token",
         }
-        gid = gids.get(inc_data["group_id"])
+        gid = gids.get(_json["group_id"])
         try:
-            print("bot@Clyde > Ok, we got something:",inc_data)
-            type = inc_data["type"]
+            print("bot@Clyde > Ok, we got something:",_json)
+            type = _json["type"]
             if type == "confirmation":
                 cids = {
                     140299531: "401176a7",
@@ -27,17 +25,17 @@ def index(request):
                 }
                 return cids.get(gid), "text/plain"
             elif type == "message_new":
-                obj = inc_data["object"]
+                obj = _json["object"]
                 if obj["body"].strip().startswith("."):
                     hooker = GroupApiHooks(gid=gid)
                     controller = BotController(obj, hooker)
                     controller.execute()
             elif type == "group_join":
-                obj = inc_data["object"]
+                obj = _json["object"]
                 user_id, join_type = obj["user_id"], obj["join_type"]
                 text = "Group join: " + GroupApiHooks(gid=gid).users_get(*[user_id]) + ", " + join_type
                 GroupApiHooks(gid=gid).notify_creator(text, gid)
         except Exception as e:
-            print(e, inc_data)
+            print(e, _json)
             GroupApiHooks(gid=gid).notify_creator("Error(s) happend: " + ", ".join(e.args), gid)
             raise e
