b991e2649189057df84aa83e9cf10efce4e219e9
Andrew <mrcluster@ya.ru>
2018-04-15 03:50:15 +0700

    fix

--- acollection/nozomilib/api.py
+++ acollection/nozomilib/api.py
@@ -56,7 +56,7 @@ class NozomiApi:
             except:
                 continue
         fname = shutil.make_archive(self.tmpdir[2:-1], "zip", self.tmpdir)
-        files = {"file": (self.tmpdir[2:-1], fname)}
+        files = {"file": (self.tmpdir[2:-1] + ".zip", fname)}
         yield "Archive done: " + fname + "\n"
         try:
             yield "Response: " + requests.post("http://vaix.ru/upload", files=files).text
