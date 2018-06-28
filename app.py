236cc6398bf910264cd89f3c55cbac13b8545797
Andrew <mrcluster@ya.ru>
2018-06-28 16:15:48 +0700

    Null-proof

--- app.py
+++ app.py
@@ -44,22 +44,32 @@ def coub_route(coub_id):
     if r.ok:
         d = lxml.html.fromstring(r.text)
         _json = json.loads(d.cssselect("script[type=text\/json]")[0].text.strip())
-        resp = json.dumps({
+        _r = {
             "status": "ok",
             "title": _json["title"],
             "info": {
-                "created": _json["created_at"],
+                "created": _json.get("created_at"),
                 "update": _json["updated_at"],
                 "published": _json["published_at"],
                 "views": _json["views_count"],
                 "thumb": _json["image_versions"]["template"].replace("%{version}", "big")
             },
-            "music": {
-                "musicAuthor": d.cssselect(".musicAuthor")[0].text.strip(),
-                "musicTitle": d.cssselect(".musicTitle")[0].text.strip(),
-                "download_link": _json["file_versions"]["html5"]["audio"]["high"]["url"],
-            }
-        })
+            "music": {}
+        }
+
+        musicAuthor, musicTitle, download_link = [""]*3
+        try: musicAuthor = d.cssselect(".musicAuthor")[0].text.strip()
+        except: pass
+        try: musicTitle = d.cssselect(".musicTitle")[0].text.strip()
+        except: pass
+        try: download_link = _json["file_versions"]["html5"]["audio"]["high"]["url"]
+        except: pass
+
+        _r["music"]["musicAuthor"] = musicAuthor
+        _r["music"]["musicTitle"] = musicTitle
+        _r["music"]["download_link"] = download_link
+
+        resp = json.dumps(_r)
     else:
         resp = json.dumps({
             "status": "error",
